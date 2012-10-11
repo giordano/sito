13f6909820cb29a26f74b895a7b806c6693517ea
Mosè Giordano <giordano.mose@libero.it>
2012-10-11 16:20:55 +0200

    riorganizzo pagina principale e personalizzo postambolo

--- sito.el
+++ sito.el
@@ -5,6 +5,20 @@
 ;; progetto.
 
 (require 'org-publish)
+
+;; Con quel poco di elisp che conosco ho definito una funzione che dovrebbe
+;; stampare correttamente il postambolo così come voglio io.  La funzione è
+;; assegnata come valore di `:html-postamble', più sotto
+(defun postambolo () (concat "<hr/>
+<p class=\"date\">Data: " date "</p>
+<p class=\"author\">Autore: " author "</p>
+<p class=\"creator\">Generato da <a href=\"http://orgmode.org/\">Org mode</a> \
+versione " org-version " con <a href=\"http://www.gnu.org/software/emacs/\">GNU \
+Emacs</a> versione " emacs-version ".  \
+Il <a href=\"https://github.com/giordano/sito\">codice sorgente</a> del sito \
+è disponibile su Github</p>
+<p class=\"xhtml-validation\">" html-validation-link "</p>\n"))
+
 (setq org-publish-project-alist
       '(
 	("sito-content"
@@ -15,5 +29,6 @@
 	 :author "Mosè Giordano"
 	 :language "it"
 	 :link-home "index.html"
+	 :html-postamble postambolo
 	 )
 	("sito" :components ("sito-content"))))
