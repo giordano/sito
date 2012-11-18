64fdf12c00a1d35eb1be7ac82b20845244d6821c
Mosè Giordano <giordano.mose@libero.it>
2012-11-18 18:37:22 +0100

    do un po' di colore al postambolo
    Ho preso ispirazione da http://gcc.gnu.org/gcc.css

--- sito.el
+++ sito.el
@@ -7,7 +7,9 @@
 ;; Con quel poco di elisp che conosco ho definito una funzione che dovrebbe
 ;; stampare correttamente il postambolo così come voglio io.  La funzione è
 ;; assegnata come valore di `:html-postamble', più sotto
-(defun postambolo-sito () (concat "<hr/>
+(defun postambolo-sito () (concat "\
+<div style=\"font-size: smaller; background: #f2f2f9; border: 2px solid #3366cc;
+            border-style: solid; border-width: thin; padding: 4px;\">
 <p class=\"author\">Autore: " author "</p>
 <p class=\"date\">Ultima modifica: " date "</p>
 <p class=\"creator\">Generato da <a href=\"http://orgmode.org/\">Org mode</a> \
@@ -15,7 +17,6 @@ versione " org-version " con <a href=\"http://www.gnu.org/software/emacs/\">GNU 
 Emacs</a> versione " emacs-version ".  \
 Il <a href=\"https://github.com/giordano/sito\">codice sorgente</a> del sito \
 è disponibile su Github</p>
-<p class=\"xhtml-validation\">" html-validation-link "</p>
 <p class=\"license\"><a rel=\"license\" \
 href=\"http://creativecommons.org/licenses/by/3.0/it/\">\
 <img alt=\"Licenza Creative Commons\" style=\"border-width:0\" \
@@ -23,7 +24,10 @@ src=\"http://i.creativecommons.org/l/by/3.0/it/80x15.png\" /></a> I contenuti \
 originali presenti su questo sito sono rilasciati sotto <a rel=\"license\" \
 href=\"http://creativecommons.org/licenses/by/3.0/it/\">licenza Creative \
 Commons Attribuzione 3.0 Italia</a>, se non \
-diversamente specificato</p>\n"))
+diversamente specificato</p>
+<p class=\"xhtml-validation\">
+<a href=\"http://validator.w3.org/check?uri=referer\">Validazione XHTML 1.0</a>
+</p></div>\n"))
 
 (setq org-publish-project-alist
       '(
